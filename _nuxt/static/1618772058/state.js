4edfd7c7f914ef49b6341cca69a9275d317b7ead
noy4 <noy4@users.noreply.github.com>
2021-04-18 18:54:20 +0000

    deploy: 165b699265f30e04d31ad1b875e428495f33cbde

---
+++ _nuxt/static/1618772058/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002Fkuwatify\u002F_nuxt\u002Fstatic\u002F1618772058",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:"\u002Fkuwatify\u002F",assetsPath:"\u002Fkuwatify\u002F_nuxt\u002F",cdnURL:a},http:{browserBaseURL:b}},globalRefs:{},ssrRefs:{"videos-videos":void 0}}}(null,"\u002F"));
